2f98d30f1960611bdc7296b87e6e6b036172cd65
mj-heath <131788422+mj-heath@users.noreply.github.com>
2025-11-13 14:56:54 +0000

    updates random_string rule to allow configuration of expected characters

--- respark/rules/generation_rules/generate_string.py
+++ respark/rules/generation_rules/generate_string.py
@@ -5,18 +5,28 @@ from ..registry import register_generation_rule
 from respark.random import randint_int, choice
 
 
-# String Rules
 @register_generation_rule("random_string")
 class RandomStringRule(GenerationRule):
     def generate_column(self) -> Column:
-        min_length = self.params.get("min_length", 0)
-        max_length = self.params.get("max_length", 50)
-        charset = self.params.get("charset", string.ascii_letters)
+        min_length: int = self.params.get("min_length", 0)
+        max_length: int = self.params.get("max_length", 50)
+        ascii_lower: bool = self.params.get("ascii_lower", True)
+        ascii_upper: bool = self.params.get("ascii_upper", True)
+        extra_char: str = self.params.get("extra_char", "")
+
+        char_set = ""
+
+        if ascii_lower:
+            char_set += string.ascii_lowercase
+        if ascii_upper:
+            char_set += string.ascii_uppercase
+        if extra_char:
+            char_set += extra_char
 
         rng = self.rng()
 
-        length = randint_int(rng, min_length, max_length, "len")
-        charset_arr = F.array([F.lit(c) for c in charset])
+        length = randint_int(rng, min_length, max_length, char_set)
+        charset_arr = F.array([F.lit(c) for c in char_set])
 
         pos_seq = F.sequence(F.lit(0), F.lit(max_length - 1))
         chars = F.transform(pos_seq, lambda p: choice(rng, charset_arr, "pos", p))
