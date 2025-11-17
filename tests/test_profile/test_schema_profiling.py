a5401319376978bf04d58f91a9908151bc312534
mj-heath <131788422+mj-heath@users.noreply.github.com>
2025-11-17 11:59:38 +0000

    fixes profiling issue with optional and integral types

--- tests/test_profile/test_schema_profiling.py
+++ tests/test_profile/test_schema_profiling.py
@@ -1,11 +1,10 @@
-import pytest
 from typing import Dict
 from respark.profile import SchemaProfile, TableProfile, profile_schema, profile_table
 
 
 def test_schema_profiling_one_schema(employees_df):
 
-    data_model = profile_schema(employees_df)
+    data_model = profile_schema({"employees": employees_df})
 
     assert isinstance(data_model, SchemaProfile)
     assert isinstance(data_model.to_dict(), Dict)
@@ -22,17 +21,3 @@ def test_schema_profiling_multiple_schema(employees_df, departments_df):
     assert data_model.tables["employees"].name == "employees"
     assert data_model.tables["departments"].name == "departments"
     assert isinstance(data_model.tables["employees"], TableProfile)
-
-
-def test_raises_error_when_passed_invalid_df_dict(employees_df, departments_df):
-
-    with pytest.raises(TypeError):
-        profile_schema(
-            {"employees": "incorrect_string", "departments": False}  # type: ignore
-        )
-    with pytest.raises(TypeError):
-        profile_schema({1: employees_df, 2: departments_df})  # type: ignore
-    with pytest.raises(TypeError):
-        profile_schema({1: "incorrect_string", 2: False})  # type: ignore
-    with pytest.raises(TypeError):
-        profile_schema({"incorrect_string"})  # type: ignore
